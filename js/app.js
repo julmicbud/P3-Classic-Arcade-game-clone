7f428f57e843b4330f12d53cedcabe5c7b63d206
Julia Hendrickson <julmicbud@gmail.com>
2016-01-27 14:58:55 -0500

    I started over...

--- js/app.js
+++ js/app.js
@@ -2,9 +2,7 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-		this.x = Math.random() * 505;
-		this.y = 63 + (Math.round(Math.random() * 2) * 83);
-		this.speed = (Math.random() * 150) + 75;
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -16,11 +14,6 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.speed * dt;
-    if(this.x >= 505) {
-    	this.y =63 + (Math.round(Math.random() * 2) * 83);
-    	this.x = -101;
-    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -31,19 +24,12 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var player = function(){
-	this.x = 0;
-	this.y = 0;
-	this.sprite = "images/car-boy.png";
-};
 
-player.update = function(){
-};
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your
