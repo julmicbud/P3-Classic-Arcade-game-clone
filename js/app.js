832685cdb1df7e910bac0c48197e08811b9d2bf2
Julia Hendrickson <julmicbud@gmail.com>
2016-01-27 14:37:31 -0500

    Added a player Funtion. It still needs more info though

--- js/app.js
+++ js/app.js
@@ -31,13 +31,16 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
+var player = function(){
+	this.x = ;
+	this.y = ;
+	this.sprite = "images/car-boy.png";
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var allEnemies
+var allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your
