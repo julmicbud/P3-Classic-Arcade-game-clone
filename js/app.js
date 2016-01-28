c6f7492c4e3b8907d5174687d8705c879197fe72
Julia Hendrickson <julmicbud@gmail.com>
2016-01-28 11:44:59 -0500

    added allEnemies array

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ Player.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your
